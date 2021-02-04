0ce04756898dfe9a9051a58ef2d7a55cf30750cd
dferes <dferes23@gmail.com>
2021-02-04 08:56:45 -0800

    added a few moret tests

--- markov.test.js
+++ markov.test.js
@@ -7,13 +7,25 @@ describe("Tests the constructor MarkovMachine", () => {
         markov1 = new markov.MarkovMachine('the cat in the hat is in the hat');
         markov2 = new markov.MarkovMachine('sally sells sea shells down by the sea shore');
     })
-    // test('Test the string passed to the constructor is turned into an arry', () => {
-    // expect().toEqual(Array);
-    // });
+    test('the string passed to the constructor is turned into an arry', () => {
+    expect(markov1.words).toBeInstanceOf(Array);
+    });
     test('the string passed to the constructor is turned into an array of correct length', () => {
         expect(markov1.words.length).toEqual(9);
         expect(markov2.words.length).toEqual(9);
     });
+    test('the words array is passed to makeChains and returns an object containing arrays', () => {
+        expect(markov1.chainObject).toBeInstanceOf(Object);
+        expect(markov1.chainObject['the']).toBeInstanceOf(Array);
+        expect(markov1.chainObject['hat']).toBeInstanceOf(Array);
+        });
+    test('the chainObject contains arrays of correct length for each key', () => {
+        expect(markov1.chainObject['the'].length).toEqual(3);
+        expect(markov1.chainObject['cat'].length).toEqual(1);
+        expect(markov1.chainObject['in'].length).toEqual(2);
+        expect(markov1.chainObject['hat'].length).toEqual(2);
+        expect(markov1.chainObject['is'].length).toEqual(1);
+        });
 });
 
 
