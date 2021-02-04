1161b1aea8fdff0f88821b7f882b47875c3f5264
dferes <dferes23@gmail.com>
2021-02-03 20:05:42 -0800

    A class implementing the logic of the markov word generator

---
+++ markov.js
@@ -0,0 +1,60 @@
+class MarkovMachine {
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    let chainObject = {};
+    const wordFrequency = this.collectWordFrequency();
+
+    for ( const [key, val] of Object.entries(wordFrequency)) {
+      chainObject[key] = [];
+      let counter = 0;
+      let index = 0;
+      let allFound = false;
+      while (!allFound) {
+        if (this.words[index] === key){
+          counter ++;
+          let nextWord =  this.words[index+1] == undefined ? null: this.words[index+1]
+          chainObject[key].push(nextWord);
+        }
+        index ++;
+        
+        allFound = counter == val ? true : false;
+      }
+    }
+    return chainObject;
+  }
+
+  collectWordFrequency() {
+    let wordFrequency = {};
+
+    for (let word of this.words) {
+      if( !(word in wordFrequency)){
+        wordFrequency[word] = this.countOccuranceInArray(this.words, word); 
+      }
+    }
+    return wordFrequency;
+  }
+
+  countOccuranceInArray = (arr, val) => arr.reduce( (a,v) => (v == val ? a+1 : a), 0);
+
+  /* return random text from chains */
+  makeText(numWords = 100) {
+    // TODO
+  }
+}
+
+
+let mm = new MarkovMachine('the cat in the hat puts the hat on the cat');
+
+console.log(mm.words);
+console.log(mm.makeChains());
